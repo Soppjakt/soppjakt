6ae8ca26463a45cf4eeff289375a835ef13af15e
Kenneth <kennb15@uia.no>
2019-11-22 13:25:14 +0100

    Master
    Master project

---
+++ Soppjakt_Master/Assets/Script/Item.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int ID;
+    public string type;
+    public string description;
+    public Sprite icon;
+    public bool pickedUp;
+
+    [HideInInspector]
+    public bool equipped;
+
+    [HideInInspector]
+    public GameObject weapon;
+    public GameObject steinsopp;
+    public GameObject sopp;
+
+    [HideInInspector]
+    public GameObject weaponManager;
+    public GameObject steinSopp;
+    public GameObject skrubbSopp;
+
+    public bool playersWeapon;
+    public bool playersSteinsopp;
+    public bool playersSkrubbsopp;
+
+
+    public void Start()
+    {
+        weaponManager = GameObject.FindWithTag("weaponManager");
+
+        if (!playersWeapon)
+        {
+            int allWeapons = weaponManager.transform.childCount;
+            for (int i = 0; i < allWeapons; i++)
+            {
+                if(weaponManager.transform.GetChild(i).gameObject.GetComponent<Item>().ID == ID)
+                {
+                    weapon = weaponManager.transform.GetChild(i).gameObject;
+                }
+            }
+        }
+        steinSopp = GameObject.FindWithTag("steinSopp");
+
+        if (!playersSteinsopp)
+        {
+            int allSopp = steinSopp.transform.childCount;
+            for (int i = 0; i < allSopp; i++)
+            {
+                if (steinSopp.transform.GetChild(i).gameObject.GetComponent<Item>().ID == ID)
+                {
+                    steinsopp = steinSopp.transform.GetChild(i).gameObject;
+                }
+            }
+        }
+        skrubbSopp = GameObject.FindWithTag("skrubbSopp");
+
+        if (!playersSkrubbsopp)
+        {
+            int allSopp = skrubbSopp.transform.childCount;
+            for (int i = 0; i < allSopp; i++)
+            {
+                if (skrubbSopp.transform.GetChild(i).gameObject.GetComponent<Item>().ID == ID)
+                {
+                    skrubbSopp = skrubbSopp.transform.GetChild(i).gameObject;
+                }
+            }
+        }
+    }
+
+    public void Update()
+    {
+        if(equipped)
+        {
+            //perform eapon acts here
+
+            if (Input.GetKeyDown(KeyCode.G))
+                equipped = false;
+
+            if (equipped == false)
+                this.gameObject.SetActive(false);
+        }
+    }
+
+    public void ItemUsage()
+    {
+        //Weapon
+
+        if(type == "Weapon")
+        {
+            weapon.SetActive(true);
+            weapon.GetComponent<Item>().equipped = true;
+        }
+
+        if( type == "Steinsopp")
+        {
+            steinsopp.SetActive(true);
+            steinsopp.GetComponent<Item>().equipped = true;
+        }
+        //Health item
+    }
+}
---
+++ Soppjakt_Master/Assets/Script/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+    private CharacterController characterController;
+    private float jumpVelocity;
+    private float rotateX = 0;
+
+    [Header("PlayerController")]
+    public float jumpSpeed;
+    public float gravity;
+
+    public float moveSpeed;
+
+    public float sensitivity;
+
+    [Header("Camera")]
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        //Rotate player
+        float rotateY = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+        transform.Rotate(new Vector3(0, rotateY, 0));
+
+        //Rotate camera
+        rotateX -= Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+        rotateX = Mathf.Clamp(rotateX, -60, 60f);
+        cam.transform.localRotation = Quaternion.Euler(rotateX, 0, 0);
+
+        //weaponPosition.localRotation = Quaternion.Euler(rotateX, 0, 0);
+
+        //Player input
+        float moveVertical = Input.GetAxis("Vertical") * moveSpeed;
+        float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed;
+
+        //Jump input
+        jumpVelocity -= gravity * Time.deltaTime;
+        if (characterController.isGrounded && Input.GetButton("Jump"))
+        {
+            jumpVelocity = jumpSpeed;
+
+        }
+
+        //Move matrix
+        Vector3 moveDirection = new Vector3(moveHorizontal, jumpVelocity, moveVertical);
+        moveDirection = transform.rotation * moveDirection;
+        //Player move - This tells the character controller to move the player.
+        characterController.Move(moveDirection * Time.deltaTime);
+
+
+    }
+}
